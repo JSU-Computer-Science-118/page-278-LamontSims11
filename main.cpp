293f7d12e7b5bd583fdfe1591c561b3ee0d0b0a8
LamontSims11 <117595824+LamontSims11@users.noreply.github.com>
2022-11-18 00:14:40 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -37,27 +37,23 @@ int main()
         numOfVolunteers++;                              
         cin >> name;                                    
     }                                                   
+       
+
+    cin >> costOfOneBox;
+                                    
+    cout << "Total number of boxes sold: "         
+         << totalNumOfBoxesSold << endl;                                                                                    
     
-    cout << endl;                                       
-    
-    cout << "Line 26: The total number of boxes sold: "         
-         << totalNumOfBoxesSold << endl;                
-         
-    cout << "Line 27: Enter the cost of one box: ";     
-    cin >> costOfOneBox;                                
-    cout << endl;                                       
-    
-    cout << "Line 30: The total money made by selling "
-         << "cookies: $"         
+    cout << "Total money made: "         
          << totalNumOfBoxesSold * costOfOneBox << endl; 
          
     if (numOfVolunteers != 0)                           
-        cout << "Line 32: The average number of "            
-             << "boxes sold by each volunteer: "             
+        cout << "Average number of "            
+             << "boxes sold by each person: "             
              << totalNumOfBoxesSold / numOfVolunteers             
              << endl;                                   
     else
-        cout << "Line 34: No input." << endl;           
+        cout << "No input." << endl;           
         
     return 0;                                           
     
